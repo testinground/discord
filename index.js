752f65e413f7c73bdf0863102f5faf7c453facf6
testinground <67091600+testinground@users.noreply.github.com>
2022-07-05 01:16:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+client.on('ready', () => {
+  console.log(`Logged in...`);
+});
+
+client.on('message', msg => {
+  msg.reply('pong');
+});
+
+client.login('token');
